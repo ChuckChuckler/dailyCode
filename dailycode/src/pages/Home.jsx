2439dac830190978f8d2542290fb3c1270e93d36
ChuckChuckler <randompeoplekid@gmail.com>
2025-05-15 18:27:09 -0400

    such a cool commit woah

---
+++ dailycode/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { useState } from "react";
+import "./Pages.css";
+
+export default function Home(){
+    return(
+        <>
+            <h1>Hiiii</h1>
+        </>
+    )
+}
