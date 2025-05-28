28cbdab7cb733fe34aecb9708e5aa8f1b08ac4a4
ChuckChuckler <randompeoplekid@gmail.com>
2025-05-27 21:36:41 -0400

    Removal of UserProfile.jsx as it is redundant

--- dailycode/src/App.jsx
+++ dailycode/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router';
 import Spawn from "./pages/Spawn.jsx";
 import UserCreation from "./pages/UserCreation.jsx";
 import Home from "./pages/Home.jsx";
-import UserProfile from "./pages/UserProfile.jsx";
 import UserSettings from "./pages/UserSettings.jsx";
 import Project from "./pages/Project.jsx";
 import Profile from "./pages/Profile.jsx";
@@ -15,7 +14,6 @@ function App(){
         <Route path="/" element={<Spawn />}></Route>
         <Route path="create-user" element={<UserCreation />}></Route>
         <Route path="home" element={<Home />}></Route>
-        <Route path="user-profile" element={<UserProfile />}></Route>
         <Route path="user-settings" element={<UserSettings />}></Route>
         <Route path="project/:id" element={<Project />}></Route>
         <Route path="profile/:user" element={<Profile />}></Route>
